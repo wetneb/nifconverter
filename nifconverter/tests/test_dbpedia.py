d874a29d6cc707f94fe10467a43f68808c80cb91
Antonin Delpeuch <antonin@delpeuch.eu>
2019-04-09 09:35:51 +0100

    Further Python 2 compatibility

--- nifconverter/tests/test_dbpedia.py
+++ nifconverter/tests/test_dbpedia.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 import unittest
 from nifconverter.dbpedia import FromDBpediaConverter
 from nifconverter.dbpedia import ToDBpediaConverter
