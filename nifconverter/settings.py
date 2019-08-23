da40ba2b6dad092f24c30e57a0e7239b51bc201b
Alex Olieman <alex@olieman.net>
2019-08-23 15:52:20 +0200

    set default STS to University of Amsterdam mirror (for now)

--- nifconverter/settings.py
+++ nifconverter/settings.py
@@ -1,3 +1,3 @@
 import os
 
-SAME_THING_SERVICE_URL = os.environ.get('SAME_THING_SERVICE_URL', 'https://global.dbpedia.org/same-thing/lookup/')
+SAME_THING_SERVICE_URL = os.environ.get('SAME_THING_SERVICE_URL', 'https://smart.hum.uva.nl/same-thing/lookup/')
